d4ba8b482c27bd3834845ef6f68f681fce731e22
Ojas Tyagi <devmegablaster@gmail.com>
2022-11-28 22:54:47 +0530

    feat: nvim autocomplete updated

--- nvim/.config/nvim/after/plugin/cmp.rc.lua
+++ nvim/.config/nvim/after/plugin/cmp.rc.lua
@@ -12,10 +12,20 @@ cmp.setup({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
+    ['<S-Tab>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true
+    }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
     }),
   }),
   sources = cmp.config.sources({
