eae47fc9f4f9583681b157f6774861b3dc5d5a29
Ojas Tyagi <devmegablaster@gmail.com>
2022-11-28 17:37:42 +0530

    feat: nvim moved nvim-tree setup to after/plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,90 +3,12 @@ require('megablaster.highlights')
 require('megablaster.maps')
 require('megablaster.plugins')
 
-local options = {
-  filters = {
-    dotfiles = false,
-    exclude = { vim.fn.stdpath "config" .. "/lua/custom" },
-  },
-  disable_netrw = true,
-  hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = { "alpha" },
-  hijack_cursor = true,
-  hijack_unnamed_buffer_when_opening = false,
-  update_cwd = true,
-  update_focused_file = {
-    enable = true,
-    update_cwd = false,
-  },
-  view = {
-    adaptive_size = true,
-    side = "left",
-    width = 25,
-    hide_root_folder = true,
-  },
-  git = {
-    enable = false,
-    ignore = true,
-  },
-  filesystem_watchers = {
-    enable = true,
-  },
-  actions = {
-    open_file = {
-      resize_window = true,
-    },
-  },
-  renderer = {
-    highlight_git = false,
-    highlight_opened_files = "none",
-
-    indent_markers = {
-      enable = false,
-    },
-
-    icons = {
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = true,
-        git = false,
-      },
-
-      glyphs = {
-        default = "",
-        symlink = "",
-        folder = {
-          default = "",
-          empty = "",
-          empty_open = "",
-          open = "",
-          symlink = "",
-          symlink_open = "",
-          arrow_open = "",
-          arrow_closed = "",
-        },
-        git = {
-          unstaged = "✗",
-          staged = "✓",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "★",
-          deleted = "",
-          ignored = "◌",
-        },
-      },
-    },
-  },
-}
-
 local has = vim.fn.has
 local is_mac = has "macunix"
 local is_win = has "win32"
 
 if is_mac then
   require('megablaster.macos')
-  require("nvim-tree").setup(options)
 end
 if is_win then
   require('megablaster.windows')
