2386202337f08806df70be17013a03204c050086
Ojas Tyagi <devmegablaster@gmail.com>
2023-03-08 23:17:05 +0530

    feat: scripts updated notes script

--- scripts/bin/notes.sh
+++ scripts/bin/notes.sh
@@ -21,14 +21,21 @@ FILE_PATH="$HOME/notes/$FILE_NAME.md"
 # Create the file
 touch $FILE_PATH
 
-# If the file is empty, add the date
-if [ ! -s $FILE_PATH ]; then
-    echo "# $DATE" >> $FILE_PATH
-fi
-
 # Open the file in nvim
-nvim -c "normal Go" \
-  -c "normal Go## $(DATE +%H:%M)" \
-  -c "normal G2o" \
-  -c "normal zz" \
-  -c "startinsert" $FILE_PATH
+if [ -z "$1" ]
+then
+
+  # If the file is empty, add the date
+  if [ ! -s $FILE_PATH ]; then
+    echo "# $DATE" >> $FILE_PATH
+  fi
+
+  nvim -c "normal Go" \
+    -c "normal Go## $(DATE +%H:%M)" \
+    -c "normal G2o" \
+    -c "normal zz" \
+    -c "startinsert" $FILE_PATH
+else
+  nvim -c "normal Go" \
+    -c "startinsert" $FILE_PATH
+fi
